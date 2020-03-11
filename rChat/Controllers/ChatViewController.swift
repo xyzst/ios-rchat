0615842dee1df40459a5976ac5a81ec748778564
Darren Rambaud <xyzst@users.noreply.github.com>
2020-03-10 21:01:57 -0500

    Update Storyboard and ChatViewController
    Add UITableViewDataSource extension to populate the UITableView with message data. Add mock message data.
    
    Related to: none

--- rChat/Controllers/ChatViewController.swift
+++ rChat/Controllers/ChatViewController.swift
@@ -14,8 +14,15 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "2@3.com", body: "Yo!"),
+        Message(sender: "1@2.com", body: "What's good?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         title = ApplicationConstants.APPLICATION_DISPLAY_NAME
         navigationItem.hidesBackButton = true
     }
@@ -32,5 +39,20 @@ class ChatViewController: UIViewController {
         } catch let e as NSError {
             print("Unable to sign out: %@", e)
         }
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let c = tableView.dequeueReusableCell(withIdentifier: ApplicationConstants.CELL_IDENTIFIER, for: indexPath)
+        
+        c.textLabel?.text = messages[indexPath.row].body
+        
+        return c
     }
 }
